4daffdcdce9ec9f1b6a69631f5e1ab08df5b6998
Dino <dbecaj@fordham.edu>
2019-11-21 13:11:13 -0500

    normalized data so that it could fit under columns in a table. Imported pandas and created a dataframe. Output csv file with columns and headers. Imported csv into sql database. Next step is to make the script create the database and automatically import the script to create the sql table.

--- repAnalysis.py
+++ repAnalysis.py
@@ -1,4 +1,6 @@
 import os
+import sqlite3
+import pandas as pd
 
 linkIndexList = []
 wantedExtensions = ['.yml', '.txt', '.md', '.cpp', '.sln', '.hpp', '.html', '.h', '.cmake',
@@ -23,6 +25,80 @@ def linkIndexFinder(linkIndexList, analysisData):
     
 #def removeExts(repAnalysisEdited, wantedExtensions, repAnalysisFinal):
     
+def dataNormalizer(analysisData, linkIndexList):
+
+    normAnalysisData = [['repName', 'repUrl', '.yml', '.txt', '.md', '.cpp',
+                        '.sln', '.hpp', '.html', '.h', '.cmake', '.pdf', '.c',
+                        '.css', '.js', '.sql', '.ttf', '.jpg', '.csv', '.mo',
+                        '.po', '.phtml', '.volt', '.gyp', '.py', '.xls', '.nsi',
+                        '.ac', '.xlsx', '.xcf', '.bat', '.lib', '.props', '.cmd',
+                        '.scm', '.java', '.pl', '.r', '.shtml', '.xlsm', '.bmp',
+                        '.eps', '.wmf', '.gif', '.doc', '.db', '.docx', '.swift',
+                        '.cshtml', '.cxx', '.pyx', '.psd', '.hh', '.htm', '.pxd',
+                        '.mxml', '.pyc', '.dot', '.mp3', '.pyd', '.pyui', '.xhtml',
+                        '.m4a']]
+
+
+    start = linkIndexList[0]
+    last = linkIndexList[len(linkIndexList)-1]
+
+    # This for loop navigates analysisData
+    for link in range(0, len(linkIndexList)-1):
+        
+        row = ['', '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0]
+        
+        if link != len(linkIndexList)-1:
+            start = linkIndexList[link]+1
+            finish = linkIndexList[link+1]-2
+            
+            row[0] = analysisData[start - 2]
+            row[1] = analysisData[start - 1]
+        else:
+            start = linkIndexList[link]         
+            finish = analysisData[len(analysisData)-1]
+            
+            row[0] = analysisData[start - 1]
+            row[1] = analysisData[start]
+         
+        
+        fileNum = ''
+        fileExt = ''
+            
+        while start != finish:
+            current = analysisData[start]              #begins analyzing file extension found after link in analysisData
+            whiteSpaceExt = current.find(' ')          #analysisData format "fileExt_numFiles", where _ is whitespace
+            whiteSpaceNum = current.rfind(' ')
+            for i in range(0, whiteSpaceExt):          #adds every character up to the whitespace
+                fileExt += current[i]
+            for j in range(whiteSpaceNum+1, len(current)):       #diff variable used bc some Extensions had extra whitespace
+                fileNum += current[j]
+            
+            fileExt = fileExt.casefold()                #makes all fileExts lowercase to prevent duplicate entries
+            numFiles = int(fileNum)
+            
+            if fileExt in normAnalysisData[0]:
+                index = normAnalysisData[0].index(fileExt)
+                row[index] = numFiles
+                
+            
+#             if fileExt in normAnalysisData:
+#                 repAnalysis[fileExt] += numFiles
+#             else:
+#                 repAnalysis[fileExt] = numFiles
+            
+            start += 1
+            fileExt = ''
+            fileNum = ''
+            
+        normAnalysisData.append(row)
+        
+        
+    
+    return normAnalysisData
+    
+    
 
 f = open("analysisData.txt", 'r')
 analysisData = f.read()
@@ -42,28 +118,29 @@ linkIndexFinder(linkIndexList, analysisData)
 print("Number of repositories:",len(linkIndexList))
 
 
-start = linkIndexList[0] + 1
+start = linkIndexList[0]
 last = linkIndexList[len(linkIndexList)-1]
 
-for i in range(0, len(linkIndexList)-1):
-    if i != len(linkIndexList)-1:
-        start = linkIndexList[i]+1
-        finish = linkIndexList[i+1]-2
+# This for loop navigates analysisData
+for link in range(0, len(linkIndexList)-1):
+    if link != len(linkIndexList)-1:
+        start = linkIndexList[link]+1
+        finish = linkIndexList[link+1]-2
     else:
-        start = linkIndexList[i]
+        start = linkIndexList[link]         
         finish = analysisData[len(analysisData)-1]
         
     while start != finish:
-        current = analysisData[start]
-        whiteSpaceExt = current.find(' ')
+        current = analysisData[start]              #begins analyzing file extension found after link in analysisData
+        whiteSpaceExt = current.find(' ')          #analysisData format "fileExt_numFiles", where _ is whitespace
         whiteSpaceNum = current.rfind(' ')
-        for i in range(0, whiteSpaceExt):
+        for i in range(0, whiteSpaceExt):          #adds every character up to the whitespace
             fileExt += current[i]
-        for j in range(whiteSpaceNum+1, len(current)):
+        for j in range(whiteSpaceNum+1, len(current)):       #diff variable used bc some Extensions had extra whitespace
             fileNum += current[j]
         
-        fileExt = fileExt.casefold()
-        numFiles = int(fileNum)
+        fileExt = fileExt.casefold()                #makes all fileExts lowercase to prevent duplicate entries
+        numFiles = int(fileNum)                  
         
         if fileExt in repAnalysis:
             repAnalysis[fileExt] += numFiles
@@ -114,6 +191,8 @@ g.write(" total files\n\n")
 print("There are", counter2, "fileExts in repAnalysisFinal")
 print("There are", sumFiles2, "total files")
 
+print(linkIndexList)
+
 
 ############
 # OUTPUT SOME BASIC STATISTICS ABOUT THE REPOSITORIES
@@ -136,76 +215,16 @@ for i in repAnalysisEdited:
     if i in wantedExtensions:
         repAnalysisFinal[i] = repAnalysisEdited[i]
 
-print(repAnalysisFinal)
+normalizedData = dataNormalizer(analysisData, linkIndexList)
+columnsPd = normalizedData[0]
 
+for i in range(2, len(columnsPd)):
+    columnsPd[i] = columnsPd[i][1:]
+print(columnsPd)
+    
+normalizedData.pop(0)
 
-################################
-# CREATING SQL DATABASE
+df = pd.DataFrame(normalizedData, columns = columnsPd)
 
-# fileExtList = []
-# for i in repAnalysisFinal:
-#     fileExtList.append(i)
-# 
-# print(fileExtList)
-# 
-# conn = sqlite3.connect('MLRA.db')
-# 
-# c = conn.cursor()
-# 
-# 
-#  
-# c.execute('''CREATE TABLE files
-#                ('.editorconfig', '.gitattributes', '.gitignore', '.gitmodules', '.yml', '.txt', '.md', '.cpp',
-#                '.sln', '.vcxproj', '.filters', '.hpp', '.html', '.h', '.cmake', '.in', '.dox', '.sh', '.config',
-#                '.cs', '.resx', '.settings', '.csproj', '.pdf', '.vb', '.vbproj', '.png', '.m4', '.w32', '.c', '.xml',
-#                '.phpt', '.inc', '.ini', '.tpl', '.php', '.dsp', '.markdown', '.lock', '.gemspec', '.rb', '.bak', '.dist',
-#                '.json', '.zep', '.example', '.gitkeep', '.css', '.js', '.sql', '.ttf', '.jpg', '.csv', '.mo', '.po',
-#                '.phtml', '.volt', '.gyp', '.pbxproj', '.xcworkspacedata', '.m', '.am', '.vcproj', '.ico', '.docs', '.py',
-#                '.xls', '.nsi', '.ac', '.xlsx', '.xcf', '.bat', '.lib', '.props', '.cmd', '.scm', '.java', '.pl', '.r',
-#                '.i', '.scala', '.shtml', '.dat', '.tex', '.zip', '.rtf', '.dev', '.1', '.el', '.def', '.types', '.xlsm',
-#                '.bmp', '.eps', '.wmf', '.gif', '.doc', '.xla', '.awk', '.manifest', '.map', '.rdf', '.spec', '.dsw',
-#                '.conf', '.guess', '.sub', '.rc', '.mc', '.bin', '.htaccess', '.db', '.properties', '.dtd', '.2', '.3', '.4',
-#                '.log', '.5', '.6', '.7', '.0', '.gz', '.ps1', '.template', '.xsl', '.tab', '.nuspec', '.docx', '.fs',
-#                '.fsproj', '.cu', '.svg', '.xcscheme', '.plist', '.swift', '.xib', '.suo', '.npmignore', '.cshtml', '.yaml',
-#                '.snk', '.dotsettings', '.vsct', '.vsixmanifest', '.xaml', '.cc', '.user', '.pump', '.dll', '.sample',
-#                '.tt', '.exe', '.vim', '.pch', '.strings', '.rst', '.mm', '.podspec', '.xcuserstate', '.mk', '.swg',
-#                '.list', '.patch', '.ml', '.mli', '.swig', '.framework', '.dylib', '.coveragerc', '.cfg', '.cxx', '.pyx',
-#                '.storyboard', '.rspec', '.pem', '.project', '.vscodeignore', '.ts', '.less', '.xsd', '.vstemplate', '.pdb',
-#                '.proj', '.asm', '.feature', '.opts', '.rake', '.ex', '.info', '.lua', '.nupkg', '.vspscc', '.scss',
-#                '.testsettings', '.s', '.texi', '.rdoc', '._', '.mdb', '.key', '.pro', '.psd', '.hh', '.erb', '.jar', '.so',
-#                '.classpath', '.prefs', '.tcc', '.ll', '.mak', '.sv', '.disabled', '.xc', '.mit', '.targets', '.ipp', '.res',
-#                '.go', '.bc', '.modulemap', '.td', '.build', '.test', '.mir', '.o', '.elf-x86-64', '.a', '.macho-x86_64',
-#                '.proftext', '.prof', '.output', '.obj', '.dsym', '.toml', '.rs', '.cache', '.orig', '.sor', '.hxx', '.out',
-#                '.prettierrc', '.eslintignore', '.vsix', '.meta', '.mat', '.asset', '.hgignore', '.il', '.htm', '.ipch',
-#                '.reference_output', '.small', '.f', '.y', '.ps', '.make', '.proto', '.wav', '.ld', '.asc', '.par',
-#                '.makefile', '.mp4', '.f90', '.bats', '.bash', '.ipynb', '.pxd', '.tcl', '.dm', '.d', '.sed', '.l', '.save',
-#                '.old', '.man', '.pm', '.t', '.as', '.exp', '.ds_store', '.rd', '.cproject', '.status', '.lo', '.swf',
-#                '.mxml', '.pyc', '.dot', '.mp3', '.ogg', '.bundle', '.inl', '.at', '.mid', '.snippet', '.dart', '.crt',
-#                '.bsd', '.diff', '.xcconfig', '.crx', '.resources', '.sch', '.sld', '.pod', '.result', '.cl', '.keep',
-#                '.data', '.hx', '.spark', '.plo', '.gypi', '.pp', '.wsdl', '.datasource', '.com', '.adb', '.ads', '.pas',
-#                '.idb', '.tlog', '.lastbuildstate', '.psp', '.isframe', '.17beta03', '.dfa', '.cvsignore', '.trig', '.ref',
-#                '.ver', '.include', '.t4', '.qml', '.err', '.sym', '.pyd', '.oom', '.vec', '.svn', '.svn-base', '.class',
-#                '.tga', '.mps', '.caches', '.history', '.eot', '.woff', '.gradle', '.xdt', '.frag', '.p7s', '.sol', '.iml',
-#                '.dbk', '.4th', '.ok', '.blk', '.adoc', '.iop', '.babelrc', '.hbs', '.vue', '.mjs', '.http', '.tsx', '.woff2',
-#                '.tmpl', '.pi', '.npmrc', '.h5', '.vhd', '.otf', '.node', '.tgz', '.eslintrc', '.jscsrc', '.htc', '.bowerrc',
-#                '.tsv', '.jshintrc', '.jsx', '.coffee', '.cls', '.nvmrc', '.pyui', '.flow', '.jj2', '.yas-parents', '.top',
-#                '.yasnippet', '.hg', '.xhtml', '.f77', '.tre', '.rsp', '.pkl', '.rsd', '.cpu', '.sm', '.em', '.sc', '.dd',
-#                '.sd', '.gd', '.nd', '.od', '.cgs', '.ms', '.shellitem', '.18', '.__subject_id_sub-01', '.__subject_id_sub-02',
-#                '.__subject_id_sub-03', '.__subject_id_sub-04', '.__subject_id_sub-05', '.__subject_id_sub-06',
-#                '.__subject_id_sub-07', '.__subject_id_sub-08', '.__subject_id_sub-09', '.__subject_id_sub-10',
-#                '.__subject_id_sub-11', '.__subject_id_sub-12', '.__subject_id_sub-13', '.__subject_id_sub-14',
-#                '.__subject_id_sub-15', '.__subject_id_sub-16', '.__runcode_1', '.__runcode_2', '.__runcode_3',
-#                '.__warpstats_linear0', '.__warpstats_linear1', '.__warpstats_linear2', '.__warpstats_linear3',
-#                '.__warpstats_linear4', '.__warpstats_linear5', '.__warpstats_linear6', '.__warpstats_linear7',
-#                '.__warpstats0', '.__warpstats1', '.__warpstats2', '.__warpstats3', '.__warpstats4', '.__warpstats5',
-#                '.__warpstats6', '.__warpstats7', '.__bbrstats0', '.__bbrstats1', '.__bbrstats2', '.__bbrstats3',
-#                '.__bbrstats4', '.__bbrstats5', '.__bbrstats6', '.__bbrstats7', '.__regtype_affine', '.__regtype_ants',
-#                '.__fwhm_0', '.__fwhm_16', '.__fwhm_32', '.__fwhm_4', '.__fwhm_8', '._dof', '.__flameo0', '.lis', '.golden',
-#                '.golden_py3', '.jst', '.apache2', '.bnf', '.cnf', '.dectest', '.mustache', '.lhpdf', '.n3', '.license',
-#                '.m4a', '.md2', '.mpd', '.ejs', '.oex', '.snap', '.qext', '.pug', '.xul', '.bcmap', '.jade', '.tld',
-#                '.blend', '.b64', '.co', '.jsm', '.io', '.io-client', '.flf', '.ctp', '.ors', '.gn', '.priv', '.pub')''')
-# 
-# conn.commit()
-# conn.execute("select * from files")
-# conn.close()
-
+print(df)
+df.to_csv('MLRAdata.csv')
