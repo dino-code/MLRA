8a2c055ebfe0cd608fdd0bc9cb4392e80eab159f
Dino <dbecaj@fordham.edu>
2020-03-19 19:03:51 -0400

    Figured out how to collect user input in a 2D vector. Now just need to create a function that does it for both matrices. After that, I need to find a way to push both matrices to python.

--- TaintExample/actions.js
+++ TaintExample/actions.js
@@ -1,18 +1,24 @@
+let row = [];
+let matrix1 = [];
 
+let val = document.getElementById('[0][0]1');
+let button = document.getElementById('button');
 
-function GetCellValues() {
-    var table = document.getElementById('table1');
-    for (var r = 0, n = table.rows.length; r < n; r++) {
-        for (var c = 0, m = table.rows[r].cells.length; c < m; c++) {
-            alert(table.rows[r].cells[c].innerHTML);
+function myFunction() {
+    for (let i = 0; i < 3; ++i) {
+        for (let j = 0; j < 3; ++j) {
+            let ID = '[' + i + '][' + j + ']1';
+            let val = document.getElementById(ID).value;
+            row.push(val);
         }
+        matrix1.push(row);
+        row = [];
     }
+
+    document.getElementById("matrix1").innerHTML = matrix1;
+
+
 }
 
-function displayVal() {
-    alert(document.getElementById('[0][0]').nodeValue)
-}
 
-let button = document.getElementById('button')
 
-button.onclick = () => displayVal();
