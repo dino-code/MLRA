7c7498c1f8a1e814429fa517c8cb74fe3799538c
Dino <dbecaj@fordham.edu>
2020-05-19 17:03:44 -0400

    changed my program to make it more detectable to PyT.

--- MultilingualMatrixMultiplier/main_prog.py
+++ MultilingualMatrixMultiplier/main_prog.py
@@ -14,31 +14,46 @@ def hello():
     if request.method == 'POST':
 
         # the POST request contains a form that the user submits. This code takes that data and places it in a dict.
-        dict = request.form.to_dict()
+#        data = request.form.to_dict()
+        data = list(request.form.to_dict().values())
+        data = [int(x) for x in data]
 
-        mat1 = []
-        mat2 = []
-        rowArr = []
-        counter1 = 0
-        counter2 = 0
+#        mat1 = []
+#        mat2 = []
+#        rowArr = []
+#        counter1 = 0
+#        counter2 = 0
+        
+        #print(type(dict.values()))
+        mmat1 = data[0:9]
+        mmat2 = data[9:18]
+        
+#        print('Look here:')
+#        print(data)
+        
+        print(mmat1, mmat2)
 
-        for i, v in dict.items():
-            if counter1 < 3:
-                rowArr.append(int(v))
-                if len(rowArr) == 3:
-                    mat1.append(rowArr)
-                    counter1 += 1
-                    rowArr = []
-            else:
-                rowArr.append(int(v))
-                if len(rowArr) == 3:
-                    mat2.append(rowArr)
-                    counter1 += 1
-                    rowArr = []
-
+#        for v in data.values():
+#
+#            if counter1 < 3:
+#                rowArr.append(int(v))
+#                if len(rowArr) == 3:
+#                    mat1.append(rowArr)
+#                    counter1 += 1
+#                    rowArr = []
+#            else:
+#                rowArr.append(int(v))
+#                if len(rowArr) == 3:
+#                    mat2.append(rowArr)
+#                    counter1 += 1
+#                    rowArr = []
+                    
         # perform matrix multiplication on the two matrices in C++ via my custom matMult module
-        mat_mult = matMult.multiply(mat1, mat2)
-
+#        mat_mult = matMult.multiply(mat1, mat2)
+        #mat_mult = matMult.multiply(mmat1, mmat2)
+        mat_mult = matMult.multiply(data)
+        print('LOOK', mat_mult)
+        #print("check it:", mat_mult)
         # if there is a POST request, the website is returned along with data. To see how the data is manipulated,
         # you can go to the javascript code and look for {{ data }} because that's where the data ends up.
         return render_template('index.html', data=mat_mult)
--- MultilingualMatrixMultiplier/matMult.cpp
+++ MultilingualMatrixMultiplier/matMult.cpp
@@ -8,17 +8,76 @@ namespace py = pybind11;
 std::vector<std::vector<int>> populateArray(py::list);
 void printVec(std::vector<std::vector<int>>);
 std::vector<std::vector<int>> matMult(std::vector<std::vector<int>>, std::vector<std::vector<int>>);
+std::vector<int> convList (py::list);
+std::vector<std::vector<int>> constr2DVec (std::vector<int>);
 
+/*
 std::vector<std::vector<int>> multiply(py::list A, py::list B) {
 	// I was unable to figure out a simpler way to unpack the Python
 	// objects passed to C++, so I copied all the integers from each 
 	// of the 2D Python lists to C++ vectors
-	std::vector<std::vector<int>> vecA = populateArray(A);
+ 
+    std::vector<std::vector<int>> vecA = populateArray(A);
 	std::vector<std::vector<int>> vecB = populateArray(B);
 	std::vector<std::vector<int>> vecC = matMult(vecA, vecB);
-
+    
+    
+  
     // returns the result of the vector multiplication.
 	return vecC;
+}
+*/
+
+std::vector<std::vector<int>> multiply(py::list pyVec) {
+    std::vector<int> data = convList(pyVec);
+    
+    std::vector<int> matOneBase;
+    for (int i = 0; i < 9; ++i) matOneBase.push_back(data.at(i));
+    //std::copy (matOneBase.begin(), data.begin(), data.begin() + 8);
+    
+    std::vector<int> matTwoBase;
+    for (int i = 9; i < data.size(); ++i) matTwoBase.push_back(data.at(i));
+    //std::copy (matTwoBase.begin(), data.begin() + 9, data.end()-1);
+    
+    std::vector<std::vector<int>> matOne = constr2DVec(matOneBase);
+    std::vector<std::vector<int>> matTwo = constr2DVec(matTwoBase);
+    
+    std::vector<std::vector<int>> matThree = matMult(matOne, matTwo);
+    
+    return matThree;
+    
+}
+
+std::vector<int> convList (py::list pyVec) {
+    std::vector<int> data;
+    
+    for (auto element : pyVec) {
+        data.push_back(element.cast<int>());
+    }
+    
+    return data;
+}
+
+std::vector<std::vector<int>> constr2DVec (std::vector<int> vec) {
+    std::vector<std::vector<int>> newVec;
+    std::vector<int> row;
+    
+    int counter = 0;
+    
+    for (int i = 0; i < vec.size(); ++i) {
+        
+        row.push_back(vec.at(i));
+        counter++;
+        
+        if (counter == 3) {
+            newVec.push_back(row);
+            row.clear();
+            counter = 0;
+        }
+        
+    }
+        
+    return newVec;
 }
 
 /*
@@ -34,6 +93,25 @@ void printVec(std::vector<std::vector<int>> vec) {
 */
 
 // this function accepts a Python list as an argument and converts it into a C++ vector.
+
+
+/*
+std::vector<std::vector<int>> newPopArray(std::vector data) {
+    std::vector<std::vector<int>> mat1;
+    std::vector<std::vector<int>> mat2;
+    std::vector<int> row;
+    matSize = 9;
+    currElement = 0;
+    
+    
+    //for (int i = 0; i < matSize; ++i) {
+        
+    //}
+    
+    return pyVec[0]
+}
+*/
+
 std::vector<std::vector<int>> populateArray(py::list pyVec) {
 	std::vector<std::vector<int>> vec;
 	std::vector<int> vecRow;
